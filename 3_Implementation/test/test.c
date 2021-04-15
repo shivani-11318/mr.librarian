c98c3667863c95b9f04fcf423d1e07d514abff79
shivani-11318 <shivanirajput500@gmail.com>
2021-04-15 14:36:04 +0530

    commit-one

--- 3_Implementation/test/test.c
+++ 3_Implementation/test/test.c
@@ -12,45 +12,7 @@ void tearDown()
 {
 }
 
-void test_create(void){
 
-    start = create_ll(start,1,1,1,1,1,GREEN);
-    TEST_ASSERT_EQUAL(1,start->uniq_id);
-    TEST_ASSERT_EQUAL(1,start->color);
-
-
-}
-
-void addBook(void){
-    
-    TEST_ASSERT_EQUAL(SUCCESS, insert_end(start,2,1,1,1,1,RED));
-    TEST_ASSERT_EQUAL(SUCCESS, insert_end(start,3,1,1,1,1,BLUE));
-    TEST_ASSERT_EQUAL(SUCCESS, insert_end(start,4,1,1,1,1,BLUE));
-    TEST_ASSERT_EQUAL(SUCCESS, insert_end(start,5,1,1,1,1,BLUE));
-    TEST_ASSERT_EQUAL(SUCCESS, insert_end(start,6,1,1,1,1,BLUE));
-    TEST_ASSERT_EQUAL(SUCCESS, insert_end(start,2,10,20,100,100,BLUE));
-}
-
-void searchBook(void){
-
-    TEST_ASSERT_EQUAL(SUCCESS, count_by_color(start,&col_data,BLUE));
-    TEST_ASSERT_EQUAL(5,col_data.num);
-}
-
-void viewBook(void){
-    TEST_ASSERT_EQUAL(SUCCESS, delete_box(start,5));
-}
-void deleteBook(void){
-    TEST_ASSERT_EQUAL(SUCCESS, display_ll(start));
-
-}
-void updateCredentials(void){
-    
-    TEST_ASSERT_EQUAL(SUCCESS, find_by_id(start,3,&res));
-    TEST_ASSERT_EQUAL(3, res.uniq_id);
-    TEST_ASSERT_EQUAL(0,find_by_id(start,312,&res));
-
-}
 
 
 int main(void)
@@ -59,13 +21,6 @@ int main(void)
     UNITY_BEGIN();
 
     /* Run Test functions */
-    RUN_TEST(test_create);
-    RUN_TEST(test_insert);
-    RUN_TEST(test_count);
-    RUN_TEST(test_find);
-    RUN_TEST(test_maxHeight);
-    RUN_TEST(test_avgVol);
-    RUN_TEST(test_diff);
     
 
 
