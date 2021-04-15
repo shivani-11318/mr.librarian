a726cc244f9d6b696ae6f76dd6f9749e7455b541
shivani-11318 <shivanirajput500@gmail.com>
2021-04-15 21:41:08 +0530

    updated

--- 2_Design/README.md
+++ 2_Design/README.md
@@ -1,1 +1,11 @@
-![Architecture Design](diagram1.png)
+* Architecture Design
+![Architecture Design](ArchitectDesign.png)
+
+* Component Design
+![Component Design](ComponentDesign.png)
+
+* Use-Case Design
+![Use-Case Design](UseCaseDesign.png)
+
+* Activity Design
+![Activity Design](ActivityDesign.png)
--- 3_Implementation/inc/header.h
+++ 3_Implementation/inc/header.h
@@ -8,20 +8,72 @@
 
 #define IN 1
 #define OUT 0
-
+/**
+ * @brief Adds a new book to the records and simultaneously update the file
+ * 
+ */
 void Addbook();
+
+/**
+ * @brief Searches for a book in the inventory
+ * 
+ */
 void Searchbook();
+
+/**
+ * @brief Displays the book's name, author, titles, state, etc.
+ * 
+ */
 void Displaybook();
+
+/**
+ * @brief Displays all books of the entered author
+ * 
+ */
 void Author();
+
+/**
+ * @brief Displays all titles of a book
+ * 
+ */
 void Titlelist();
+
+/**
+ * @brief Gives the count of books, issued and in-stock
+ * 
+ */
 void Stock();
+
+/**
+ * @brief Borrows a book and update the book's state in inventory
+ * 
+ */
 void Issue();
+
+/**
+ * @brief Returns a book and update the book's state in inventory
+ * 
+ */
 void bookret();
+
+/**
+ * @brief Adds a new member to the records and simultaneously update the file 
+ * 
+ */
 void Addmembr();
+
+/**
+ * @brief Function to exit the menu
+ * 
+ */
 void Exit();
 
 char info[500];
 
+/**
+ * @brief Structure of a book
+ * 
+ */
 struct
 {
 	int bid;
@@ -31,6 +83,11 @@ struct
 	char titles[500];
 	int status;
 } book;
+
+/**
+ * @brief Structure of a member
+ * 
+ */
 struct
 {
 	int mid;
@@ -47,8 +104,8 @@ FILE *librecord;
 FILE *membrrecord;
 FILE *fp1;
 FILE *fp2;
-FILE *temp1;
-FILE *temp2;
+FILE *temp1; 
+FILE *temp2; 
 
 
 #endif
